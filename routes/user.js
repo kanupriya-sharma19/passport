b4dda1d03ebd5240912320d6dffdc9ac106b056f
kanupriya-sharma19 <sharmakanpuriya@gmail.com>
2025-02-08 16:17:28 +0530

    second

--- routes/user.js
+++ routes/user.js
@@ -9,14 +9,19 @@ import passport from "passport";
 
 export const user = express.Router();
 user.route("/signup").post(postUser);
-user.route("/login").post(
-  passport.authenticate("local", {
-    failureMessage: "Invalid credentials",
-  }),
-  (req, res) => {
-    res.json({ message: "Login successful", user: req.user });
-  }
-);
+user.route("/login").post((req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) return res.status(500).json({ error: "Internal server error" });
+    if (!user) return res.status(401).json({ error: info?.message || "Invalid credentials" });
+
+    req.logIn(user, (err) => {
+      if (err) return res.status(500).json({ error: "Login failed" });
+
+      res.json({ message: "Login successful", user });
+    });
+  })(req, res, next);
+});
+
 
 user.get("/logout", logOut);
 
